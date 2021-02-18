55616c8bd8ff77c8535acbe9e100864ab53ba308
MartinSanchezIut <martin.sanchez.iut@gmail.com>
2021-02-18 16:23:39 +0100

    avancement crawler terl3

--- HLIN601-TERL3/MyCrawler.py
+++ HLIN601-TERL3/MyCrawler.py
@@ -1,6 +1,7 @@
 import requests
 import time
 import socket
+from urllib.parse import urlparse
 from bs4 import BeautifulSoup
 
 DEBUG = True
@@ -10,17 +11,21 @@ DEBUG_REQUEST = True
 def inspect(url, deep):
     global DEBUG
 
-    if url[len(url) - 1] == "/":
-        url = url[0:len(url) - 1]
+    # if url[len(url) - 1] == "/":
+    #     url = url[0:len(url) - 1]
     if DEBUG: print("URL: " + url)
+
+    o = urlparse(url)
+    domain = o.netloc.removeprefix("www.")
+    if DEBUG: print("O : " + str(o) + "\n")
 
     start = time.time()
     if DEBUG: print("Start at: " + str(start))
 
     visitedCount = []
     allUrlVisited = []
-    if DEBUG: print("visitedCount: ".join(visitedCount))
-    if DEBUG: print("allUrlVisited: ".join(allUrlVisited))
+    if DEBUG: print("visitedCount: " + str(visitedCount))
+    if DEBUG: print("allUrlVisited: " + str(allUrlVisited) + "\n")
 
     listOfUrl = [url]
     profondeur = deep
@@ -41,13 +46,16 @@ def inspect(url, deep):
             linkToInspect = url + linkToInspect
 
         if DEBUG: print("Profondeur : " + str(profondeur) + "\n")
-        if profondeur > 0 and (linkToInspect.endswith(".html") or linkToInspect.endswith(".php")):
-            returnList = listOfLinksOf(linkToInspect)
+        if profondeur > 0 and urlIsValid(linkToInspect):
+            # (linkToInspect.endswith(".html") or linkToInspect.endswith(".php")
+            #  or not linkToInspect.endswith(".*")):   Le truc a modifier
+
+            returnList = listOfLinksOf(linkToInspect, domain)
             profondeur -= 1
 
             for x in returnList:
                 if not x in allUrlVisited:
-                    listOfUrl.append(x)
+                    listOfUrl.insert(0, x)
 
     end = time.time()
     if DEBUG: print("Ends at: " + str(end))
@@ -55,14 +63,15 @@ def inspect(url, deep):
     file = open("Result.txt", 'w+')
     file.write("Task ended in " + str(end - start) + " \n\n")
     for element in range(0, len(allUrlVisited)):
+        file.write("'" + allUrlVisited[element] + "',\n")
         amountOfSpace = 100 - len(str(allUrlVisited[element]))
-        file.write(
-            str(allUrlVisited[element]) + (" " * amountOfSpace) + " : Visited " + str(visitedCount[element]) + "\n")
+        # file.write(
+        #    str(allUrlVisited[element]) + (" " * amountOfSpace) + " : Visited " + str(visitedCount[element]) + "\n")
 
     return 1
 
 
-def listOfLinksOf(url):
+def listOfLinksOf(url, domain):
     global DEBUG_REQUEST
 
     urlToCrawl = []
@@ -73,20 +82,33 @@ def listOfLinksOf(url):
         links = soup.find_all('a')
 
         urls = []
+        trash = []
         for tag in links:
             link = tag.get('href', None)
-            if link is not None and not link.startswith("#") and not link == "/" and not link.startswith("mailto:"):
-                if link.startswith(url):
-                    link = link[len(url):len(link)]
-                urls.append(link)
+
+            if link.startswith("/") and not link == "/":
+                link = url[:-1] + link
+            newLinkData = urlparse(link)
+
+            if newLinkData.netloc.__contains__(domain):
+                if link is not None and not link.startswith("#") and not link.startswith("mailto:"):
+                    # if link.endswith("/"):
+                    #    link = link[:-1]
+                    urls.append(link)
+            else:
+                trash.append(link)
 
         urlToCrawl = list(dict.fromkeys(urls))
 
         if DEBUG_REQUEST:
-            print("    " + url + "\n    ---------------------------------------  \n")
+            print("    " + url + "\n    ---------------------------------------")
             fileUrls = ""
             for x in urlToCrawl:
                 fileUrls += "   " + x + " |"
+            print(fileUrls)
+            fileUrls = "   Trash : "
+            for y in trash:
+                fileUrls += "   " + y + " |"
             print(fileUrls)
             print("    ---------------------------------------  \n\n")
 
@@ -94,6 +116,20 @@ def listOfLinksOf(url):
         if DEBUG_REQUEST: print("   " + url + ": Error\n")
 
     return urlToCrawl
+
+
+def urlIsValid(url):
+    tab = url.split("/")
+    last = tab[-1]
+    lastReversed = tab[-1][::-1]
+    for i in range(0, len(lastReversed)):
+        if lastReversed[i] == ".":
+            # On trouve un point avant un /
+            # Verifier les terminaisons
+            if not last.endswith(".html") or last.endswith(".php"):
+                return False
+
+    return True
 
 
 def getIpOf(host):
@@ -108,10 +144,67 @@ def getIpOf(host):
     return ip
 
 
+def verif():
+    urlToValid = ['http://www.lirmm.fr/',
+                  'http://www.lirmm.fr/switchlanguage/to/lirmm_eng/65',
+                  'http://www.lirmm.fr/rssfeed/news',
+                  'http://www.lirmm.fr/annuaire',
+                  'https://webmail.lirmm.fr/',
+                  'https://intranet.lirmm.fr/xml/in/0100-07.html',
+                  'http://www.lirmm.fr/Partenariats-entreprises',
+                  'http://www.lirmm.fr/Scientifiques-institutions',
+                  'http://www.lirmm.fr/acces-etudiants',
+                  'http://www.lirmm.fr/le-lirmm/presentation',
+                  'http://www.lirmm.fr/le-lirmm/organisation',
+                  'http://www.lirmm.fr/le-lirmm/chiffres-cle',
+                  'http://www.lirmm.fr/le-lirmm/historique',
+                  'http://www.lirmm.fr/le-lirmm/partenariats',
+                  'http://www.lirmm.fr/le-lirmm/emplois',
+                  'http://www.lirmm.fr/le-lirmm/les-services',
+                  'http://www.lirmm.fr/actualites',
+                  'http://www.lirmm.fr/agenda',
+                  'http://www.lirmm.fr/recherche/departements',
+                  'http://www.lirmm.fr/recherche/equipes',
+                  'http://www.lirmm.fr/recherche/plateformes',
+                  'http://www.lirmm.fr/recherche/publications',
+                  'http://www.lirmm.fr/recherche/doctorants',
+                  'http://www.lirmm.fr/recherche/theses-et-hdr-soutenues',
+                  'http://www.lirmm.fr/recherche/conferences-et-ecoles-thematiques',
+                  'http://www.lirmm.fr/recherche/sujets-de-these-proposes',
+                  'http://www.lirmm.fr/innovation/nos-competences',
+                  'http://www.lirmm.fr/innovation/partenariats-industriels',
+                  'http://www.lirmm.fr/innovation/conseils-et-expertises',
+                  'http://www.lirmm.fr/innovation/seminaires',
+                  'http://www.lirmm.fr/innovation/creation-d-entreprises',
+                  'http://www.lirmm.fr/international/international',
+                  'http://www.lirmm.fr/contact-acces',
+                  'http://www.lirmm.fr/recherche/departements/info',
+                  'http://www.lirmm.fr/recherche/plateformes/plateforme-informatique',
+                  'http://www.lirmm.fr/recherche/departements/rob',
+                  'http://www.lirmm.fr/recherche/plateformes/plateforme-robotique',
+                  'http://www.lirmm.fr/recherche/departements/mic',
+                  'http://www.lirmm.fr/recherche/plateformes/plateforme-microelectronique',
+                  'http://www.lirmm.fr/actualites/felicitations-a-neocean-pour-le-grand-prix-de-l-innovation-de-l-occitanie-2021',
+                  'http://www.lirmm.fr/actualites/news-ninjalab-startup-hebergee-au-lirmm',
+                  'http://www.lirmm.fr/actualites/video-euronews-sur-les-robots-paralleles-a-cables-resultat-du-projet-europeen-hephaestus',
+                  'http://www.lirmm.fr/le-lirmm/organisation/les-services/dist',
+                  'https://seafile.lirmm.fr/',
+                  'https://gite.lirmm.fr',
+                  'http://www.lirmm.fr/le-lirmm/organisation/les-services/comm',
+                  'https://www.lirmm.fr/lirmm_admin',
+                  'https://www.lirmm.fr/extranet/manuel-redaction-web-LIRMM.pdf',
+                  'http://www.lirmm.fr/content/view/sitemap/2',
+                  'http://www.lirmm.fr/informations/informations-legales',
+                  'https://intranet.lirmm.fr/xml/in/0309-29.html']
+
+    for i in range(0, len(urlToValid)):
+        print(str(urlToValid[i]) + "  =   " + str(urlIsValid(urlToValid[i])))
+
+
 if __name__ == "__main__":
     # url = input("Entrez l'url: ")
 
     getIpOf("www.lirmm.fr")
-
-    print(inspect("http://www.lirmm.fr/", 50))
+    verif()
+    # print(inspect("http://www.lirmm.fr/", 1))
     # print(listOfLinksOf("http://www.lirmm.fr/"))
